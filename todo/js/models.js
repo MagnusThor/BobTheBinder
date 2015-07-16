737e0d78fa6ee53ce7e474702db82a774d78570d
Magnus Thor <magnus@xsockets.net>
2015-07-16 13:12:43 +0200

    Examples updated

--- todo/js/models.js
+++ todo/js/models.js
@@ -8,31 +8,22 @@
         this.addTodo = function (evt) {
             evt.preventDefault();
             var todo = new TodoItemViewModel(self.todo.id, self.todo.task, self.todo.completed);
-
             // self.todos.push(todo);
-
             callbacks.onadd.apply(todo);
-
-
             self.todo.reset();
         };
-
         this.removeTodo = function (evt) {
             evt.preventDefault();
             var todo = this;
-          
             callbacks.ondelete.apply(todo);
             //  if (index >= 0) self.todos.splice(index, 1);
         };
         this.todo = new TodoItemViewModel();
     }
-
     return model;
-
 })();
 
 var TodoItemViewModel = (function () {
-
     var model = function (id, task, completed) {
         this.id = id || new Date();
         this.task = task || "";
@@ -44,5 +35,4 @@ var TodoItemViewModel = (function () {
         };
     };
     return model;
-
 })();
